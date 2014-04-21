1b7f2fd80ea088eeb6c0b528cbfbcb4ee163cb09
Kuba Orlik <groovy354@gmail.com>
2014-04-21 08:05:59 +0200

    correction 2

--- tree.js
+++ tree.js
@@ -121,7 +121,7 @@ SVGTree.Canvas = function(canvas_element, width, height){
 	}
 
 	this.draw = function(){
-		this.svgNode.innerHTML("");
+		this.svgNode.innerHTML = "";
 		this.Tree.draw(this.svgNode);
 	}
 }
